222c65a08d277610ae92bfafa66fb00737120bbd
Tony <89453090+techtony92@users.noreply.github.com>
2023-08-02 02:20:29 -0400

    Added information about node specific environments

---
+++ 1.Introduction/3.Executing Node/index.js
@@ -0,0 +1,10 @@
+/**
+ * * NodeJS 
+ * 
+ * * NodeJs is a environment were javascript code runs. This is different from browsers
+ * * Node does not have the same API's that browsers do, do no alert(), no window, etc etc.
+ * * Very important to understand where your javascript code is gonna be ran, because that 
+ * * environment determines what API's are available for you to use.
+ * * Anything DOM related does not work in nodeJS. 
+ * *
+*/
